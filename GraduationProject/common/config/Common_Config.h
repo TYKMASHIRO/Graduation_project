77c3232cd883b65a5a1dd24ddf13d72885a38a9a
mashiro <1906278302@qq.com>
2024-09-24 23:31:48 +0800

    init

---
+++ GraduationProject/common/config/Common_Config.h
@@ -0,0 +1,9 @@
+#ifndef __COMMON_CONFIG_H
+#define __COMMON_CONFIG_H
+
+#define GPS_INFO_MODE "PCAS04,3"
+#define GPS_INFO_UPDATE_FREQ "PCAS02,1000"
+#define GPS_INFO_BEGIN "GGA"
+#define GPS_INFO_END "TXT"
+
+#endif 
